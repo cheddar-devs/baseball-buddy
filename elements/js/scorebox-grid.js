6cb43de80f747db6cd8b9cf5cabb1fc241cde926
Mike Cheddar <kogcheddarxm@gmail.com>
2016-03-03 16:32:08 -0500

    Grabs current days scores
    Viable now that preseason has started

--- elements/js/scorebox-grid.js
+++ elements/js/scorebox-grid.js
@@ -23,5 +23,24 @@ Polymer({
       return "";
 
     return "isFavorite";
+  },
+  getCurrentDay:  function(){
+    var currentDate = new Date();
+
+    var month = currentDate.getMonth() + 1;
+
+    if(month < 10)
+      month = "0" + month;
+
+    var year = currentDate.getFullYear();
+
+    var day = currentDate.getDate();
+
+    if(day < 10)
+      day = "0" + day;
+
+    return "year_" + year + "/month_" + month + "/day_" + day;
+
+    //"year_2015/month_03/day_28"
   }
 });
